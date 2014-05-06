2dddc47526b363ef2c14737518b2c2b900ff3193
mdmudassir <user1@Administrator9.local>
2014-05-06 14:12:20 -0400

    Todays Class
    Objective C Course

---
+++ TodaysClass/TodaysClass/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TodaysClass
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 mudassir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
