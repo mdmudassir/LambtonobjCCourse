85153a1d612d33a76f4f9818030e61f1005e7b3e
mdmudassir <user1@Administrator9.local>
2014-05-06 14:21:52 -0400

    update

--- TodaysClass/TodaysClass/main.m
+++ TodaysClass/TodaysClass/main.m
@@ -14,8 +14,9 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int numerator = 1;
+        int denominator = 3;
+        NSLog(@"The fraction is %i/%i", numerator,denominator);
     }
     return 0;
 }
